9c843d5a9598a1de7a8654603c2b90ffd7935c4a
Daniel Nogueira <danielsilvangr@gmail.com>
2024-09-22 16:43:08 -0300

    Refatora código

--- jokenpo.py
+++ jokenpo.py
@@ -1,62 +1,62 @@
-'''
+"""
 Jogue "Pedra, papel ou tesoura" contra o computador.
-'''
+"""
 
 import random
 
 from time import sleep
 
 # Título do jogo
-print('=' * 30)
-msg = 'JOKENPÔ GAME'
-print(f'{msg:^30}')
-print('=' * 30)
+print("=" * 30)
+msg = "JOKENPÔ GAME"
+print(f"{msg:^30}")
+print("=" * 30)
 print()
 
 # Escolha do usuário
-print('[1] Pedra [2] Papel [3] Tesoura')
-u = (input('Escolha: '))
+print("[1] Pedra [2] Papel [3] Tesoura")
+u = input("Escolha: ")
 
 # Escolha do computador
-lista = ['1', '2', '3']
+lista = ["1", "2", "3"]
 random.shuffle(lista)
 c = random.choice(lista)
 
 # Animação
 print()
-print('JO')
+print("JO")
 sleep(1)
-print('KEN')
+print("KEN")
 sleep(1)
-print('PÔ!')
+print("PÔ!")
 sleep(1)
 
 # Análise do vencedor
-if u == '1':
-    if c == '1':
-        print('\nEmpate')
-    elif c == '2':
-        print('\nComputador ganhou!')
-    elif c == '3':
-        print('\nVocê ganhou!')
-elif u == '2':
-    if c == '2':
-        print('\nEmpate')
-    elif c == '1':
-        print('\nVocê ganhou!')
-    elif c == '3':
-        print('\nComputador ganhou!')
-elif u == '3':
-    if c == '3':
-        print('\nEmpate')
-    elif c == '1':
-        print('\nComputador ganhou!')
-    elif c == '2':
-        print('\nVocê ganhou!')
+if u == "1":
+    if c == "1":
+        print("\nEmpate")
+    elif c == "2":
+        print("\nComputador ganhou!")
+    elif c == "3":
+        print("\nVocê ganhou!")
+elif u == "2":
+    if c == "2":
+        print("\nEmpate")
+    elif c == "1":
+        print("\nVocê ganhou!")
+    elif c == "3":
+        print("\nComputador ganhou!")
+elif u == "3":
+    if c == "3":
+        print("\nEmpate")
+    elif c == "1":
+        print("\nComputador ganhou!")
+    elif c == "2":
+        print("\nVocê ganhou!")
 else:
-    print('\nOpção inválida')
+    print("\nOpção inválida")
     exit()
 
 # Escolhas
 print()
-print(f'Usuário escolheu [{u}] Computador escolheu [{c}]')
+print(f"Usuário escolheu [{u}] Computador escolheu [{c}]")
